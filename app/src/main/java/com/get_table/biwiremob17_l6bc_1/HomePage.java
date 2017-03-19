c523aa10fc60532c2c8a7394bc31db639d6d82fc
AlbertCahyawan <albertcahyawan@hotmail.com>
2017-03-19 23:26:54 +0700

    First commit

---
+++ app/src/main/java/com/get_table/biwiremob17_l6bc_1/HomePage.java
@@ -0,0 +1,13 @@
+package com.get_table.biwiremob17_l6bc_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
