9d43a4d06f85ca2f6fffe8ae1b794868c0159991
AlbertCahyawan <albertcahyawan@hotmail.com>
2017-04-25 18:23:35 +0700

    -Fifth Commit Added Registration and Login Fragmentation
    -Multiple Login Fragmented
    -Registration Added

---
+++ app/src/main/java/com/get_table/biwiremob17_l6bc_1/OrderFragement.java
@@ -0,0 +1,26 @@
+package com.get_table.biwiremob17_l6bc_1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by albertcahyawan on 4/4/2017.
+ */
+
+public class OrderFragement extends  Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.order_layout, container, false);
+        return myView;
+    }
+
+
+}
